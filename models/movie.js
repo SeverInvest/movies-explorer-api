a5d02137e7d6e4bfba530d7ca60940081905ccf6
Николай Васильев <nikolay.vasilyev@uksever-invest.ru>
2023-04-10 18:09:08 +0300

    add models, statuses, errors

---
+++ models/movie.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+const isURL = require('validator/lib/isURL');
+
+mongoose.set('strictQuery', false);
+
+const movieSchema = new mongoose.Schema({
+
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (url) => isURL(url),
+      message: 'incorrect link format',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (url) => isURL(url),
+      message: 'incorrect link format',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (url) => isURL(url),
+      message: 'incorrect link format',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  movieId: {
+    type: String,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+
+});
+
+module.exports = mongoose.model('movie', movieSchema);
---
+++ models/user.js
@@ -0,0 +1,51 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+mongoose.set('strictQuery', false);
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (email) => isEmail(email),
+        message: 'incorrect email',
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      default: ' ',
+    },
+
+  },
+);
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new UnauthorizedError('Incorrect password or email');
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new UnauthorizedError('Incorrect password or email');
+          }
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
