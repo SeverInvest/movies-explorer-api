ad31247478d86b4c217048a8d34fa29e7375efdf
Николай Васильев <nikolay.vasilyev@uksever-invest.ru>
2023-04-17 18:54:17 +0300

    change name of the database

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ module.exports = {
   port: process.env.PORT || 3001,
   nodeEnv: process.env.NODE_ENV || 'production',
   jwtSecret: process.env.JWT_SECRET || 'some-secret-key',
-  addressDB: process.env.DB_ADDRESS || 'mongodb://127.0.0.1/mestodb',
+  addressDB: process.env.DB_ADDRESS || 'mongodb://127.0.0.1/bitfilmsdb',
   addressCors: process.env.CORS_ADDRESS || 'http://localhost:3000',
 };
