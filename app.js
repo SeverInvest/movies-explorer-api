4f4cab5e441c9ada71c5015f6f085e58c73c0508
Николай Васильев <nikolay.vasilyev@uksever-invest.ru>
2023-04-19 11:47:41 +0300

    убрал rate-limit

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 // const { port, addressCors, addressDB } = require('./config');
 const { addressCors, addressDB } = require('./config');
 const router = require('./routes/index');
-const limiter = require('./middlewares/rateLimit');
+// const limiter = require('./middlewares/rateLimit');
 
 const handleError = require('./middlewares/handleError');
 
 mongoose.set('strictQuery', false);
 
 const app = express();
-app.set('trust proxy', 2);
+// app.set('trust proxy', 2);
 app.use(helmet());
 app.use(express.json());
 mongoose.connect(addressDB);
@@ -22,7 +22,7 @@ app.use(cors({
   origin: addressCors,
 }));
 app.use(requestLogger);
-app.use(limiter);
+// app.use(limiter);
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
