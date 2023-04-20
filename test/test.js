585380cc43e29dc601f8427ef5d4b2d8a6b91467
Николай Васильев <nikolay.vasilyev@uksever-invest.ru>
2023-04-20 08:25:22 +0300

    done tests endpoints

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const request = require('supertest');
 const app = require('../app');
 const User = require('../models/user');
+const Movie = require('../models/movie');
 
 describe('Пробуем, что тесты работают', () => {
   it('проверка работоспособности фреймворка', () => {
@@ -20,21 +21,63 @@ const testLogin = {
   email: 'email@test.net',
 };
 
+const testUser2 = {
+  name: 'test_user2',
+  password: 'test_password2',
+  email: 'email2@test.net',
+};
+
+const testLogin2 = {
+  password: 'test_password2',
+  email: 'email2@test.net',
+};
+
 const testChangeUser = {
   name: 'test_1_user',
   email: 'email_1@test.net',
 };
 
+const testMovie = {
+  country: 'Russia',
+  director: 'Harry Potter',
+  duration: '123',
+  year: '1990',
+  description: 'Самый лучший фильм-2',
+  image: 'https://mobimg.b-cdn.net/v3/fetch/bc/bc45d1305c40e2ec7d72c71080b34751.jpeg',
+  trailerLink: 'https://youtu.be/aNG-QfgwntU',
+  thumbnail: 'https://mobimg.b-cdn.net/v3/fetch/bc/bc45d1305c40e2ec7d72c71080b34751.jpeg',
+  movieId: '1234',
+  nameRU: 'МЫ',
+  nameEN: 'They',
+};
+
+const testBrokenMovie = {
+  country: 'Russia',
+  director: 'Harry Potter',
+  duration: '123',
+  year: '1990',
+  description: 'Самый лучший фильм-2',
+  image: '',
+  trailerLink: '',
+  thumbnail: 'https://',
+  movieId: '1234',
+  nameRU: 'МЫ',
+  nameEN: 'They',
+};
+
 const testBrokenToken = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDNlZWViNjcwYTE3NWZkMDA4ODk1MzEiLCJpYXQiOjE2ODE5MDIwMTksImV4cCI6MTY4MjUwNjgxOX0.nCL-hMzDcBRFIzMEwxZVjMoSXVp3LBsghtb1i77GOBg';
 let testToken;
+let testIdMovie;
 
 describe('проверка ендпоинтов', () => {
   before(async () => { // чистим базу до тестов
     await User.deleteMany();
+    await Movie.deleteMany();
   });
 
   after(async () => { // чистим базу после тестов
     await User.deleteMany();
+    await Movie.deleteMany();
   });
 
   describe('Регистрация пользователя', () => {
@@ -84,6 +127,64 @@ describe('проверка ендпоинтов', () => {
       assert.equal(res.status, 200);
       assert.equal(res.body.name, 'test_1_user');
       assert.equal(res.body.email, 'email_1@test.net');
+    });
+  });
+
+  describe('Добавление фильма', () => {
+    it('добавить фильм с валидной информацией и правильным токеном', async () => {
+      const res = await request(app).post('/movies').send(testMovie).set({
+        authorization: testToken,
+      });
+      assert.equal(res.status, 201);
+      assert.equal(res.body.country, 'Russia');
+      assert.equal(res.body.nameRU, 'МЫ');
+      testIdMovie = res.body._id;
+    });
+    it('отказать в добавлении фильма с невалидной информацией, но правильным токеном', async () => {
+      const res = await request(app).post('/movies').send(testBrokenMovie).set({
+        authorization: testToken,
+      });
+      assert.equal(res.status, 400);
+    });
+    it('отказать в добавлении фильма с валидной информацией, но неправильным токеном', async () => {
+      const res = await request(app).post('/movies').send(testMovie).set({
+        authorization: testBrokenToken,
+      });
+      assert.equal(res.status, 401);
+    });
+  });
+
+  describe('Получить фильмы', () => {
+    it('получить список фильмов (правильный токен)', async () => {
+      const res = await request(app).get('/movies').set({
+        authorization: testToken,
+      });
+      assert.equal(res.status, 200);
+    });
+  });
+
+  describe('Удаление фильма', () => {
+    it('удалить фильм с правильным id_movie и правильным токеном', async () => {
+      const res = await request(app).delete(`/movies/${testIdMovie}`).set({
+        authorization: testToken,
+      });
+      assert.equal(res.status, 200);
+    });
+    it('отказать в удалении фильма с правильным id_movie, но чужим токеном', async () => {
+      // добавляем фильм под пользователем 1
+      const res2 = await request(app).post('/movies').send(testMovie).set({
+        authorization: testToken,
+      });
+      testIdMovie = res2.body._id;
+      // регистрируем пользователя 2 и получаем токен
+      await request(app).post('/signup').send(testUser2);
+      const res = await request(app).post('/signin').send(testLogin2);
+      const testToken2 = `Bearer ${res.body.token}`;
+      // удаляем фильм 1-го пользователя, используя токен 2-го пользователя
+      const res1 = await request(app).delete(`/movies/${testIdMovie}`).set({
+        authorization: testToken2,
+      });
+      assert.equal(res1.status, 403);
     });
   });
 });
